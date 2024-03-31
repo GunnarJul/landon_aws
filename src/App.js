58bad17b8439ed2249f47323264b70308307b98c
Gunnar Jul Jensen <gunnarjuljensen@gmail.com>
2024-03-31 17:39:01 +0200

    structureal changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <b>Gjj rock the boat</b>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
